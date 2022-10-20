f7c2905d30dc2f80132637500efff4b241dbb381
ethanvdv <ethan.vandervegt@gmail.com>
2022-10-20 20:54:37 +1000

    Updated KT-Tensor

--- v-sigma-KT-Tensor.py
+++ v-sigma-KT-Tensor.py
@@ -1,5 +1,4 @@
 import numpy as np
-from PIL import Image
 import matplotlib.pyplot as plt
 from phantominator import shepp_logan
 from einops import rearrange, repeat
@@ -41,25 +40,57 @@ def kaleidoscope(img, nu, sigma):
             values1[r] = m1
             values2[c] = m2
     
+    #Turn them into tensors
+    rowschange = torch.tensor(values1.copy(), dtype=torch.int64)
+    rowschange = repeat(rowschange,'h -> h c', c = h)
+
+    #Need to add an extra dimension since gather removes ones
+    rowschange = rearrange(rowschange, 'h w -> 1 1 1 h w')
+
+    colschange = torch.tensor(values2.copy(), dtype=torch.int64)
+    colschange = repeat(colschange,'h -> h c', c = w)
+    
+    #Need to add an extra dimension since gather removes ones
+    colschange = rearrange(colschange, 'h w -> 1 1 1 w h')
+
+    changes = torch.cat((rowschange, colschange), 0)
     
     
-    return (values1, values2)
+    #returns as (dummy) b c h w
+    return changes
 
 
-# def tensorKaleidoscope()
+def tensorKaleidoscope(img, changes):
+    
+    #Apply the change in rows
+    rows = torch.gather(img, 2, changes[0,:,:,:,:])
+
+    #Apply the change in columns
+    output = torch.gather(rows, 3, changes[1,:,:,:,:])
+    
+    return output
 
 
 
 N = 176
+numCh = 1
+
+#From Marlon's Code for loading Shepp_Logan
+
+ph = np.rot90(np.transpose(np.array(shepp_logan(N))), 1)
+ph = torch.tensor(ph.copy(), dtype=torch.float)
+ph = torch.unsqueeze(ph, 0)
+ph = torch.cat([ph]*numCh, 0)
+ph = torch.unsqueeze(ph, 0)
+
 
 #Lets make the integer mask
-img = np.arange(N*N)
+indexes = np.arange(N*N)
 
 #Reshape to square
-img = np.reshape(img, [N,N])
+indexes = np.reshape(indexes, [N,N])
 
 #Chose nu and sigma
-
 #downscaling
 nu = 15
 
@@ -67,42 +98,17 @@ nu = 15
 sigma = 17
 
 #Returns the indexes of the "rows" and the indexes of the "cols"
-values1, values2 = kaleidoscope(img, nu, sigma)
+changes = kaleidoscope(indexes, nu, sigma)
 
-#Turn them into tensors
-rowschange = torch.tensor(values1.copy(), dtype=torch.int64)
-rowschange = repeat(rowschange,'h -> h c', c = N)
-rowschange = rearrange(rowschange, 'h w -> 1 1 h w')
-
-colschange = torch.tensor(values2.copy(), dtype=torch.int64)
-colschange = repeat(colschange,'h -> h c', c = N)
-colschange = rearrange(colschange, 'h w -> 1 1 w h')
+#saves on having to recreate this multiple times!
 
 
-
-
-#From Marlon's Code for loading Shepp_Logan
-numCh = 1
-# Generate phantom for testing
-ph2 = np.rot90(np.transpose(np.array(shepp_logan(N))), 1)
-# As pytorch tensor
-ph = torch.tensor(ph2.copy(), dtype=torch.float)
-# Create channel dim (with dummy data for the channel dimension)
-ph = torch.unsqueeze(ph, 0)
-ph = torch.cat([ph]*numCh, 0)
-# Create batch dim
-ph = torch.unsqueeze(ph, 0)
-
-
-#Apply the change in rows
-output = torch.gather(ph, 2, rowschange)
-
-#Apply the change in columns
-output = torch.gather(output, 3, colschange)
+output = tensorKaleidoscope(ph, changes)
 
 plt.figure(1)
 plt.imshow(np.abs(output[0,0,:, :]))
 plt.show()
+
 
 
 
